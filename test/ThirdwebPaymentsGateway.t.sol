08a3ad5734c6194135105be09f9e25da8e55dc35
Yash <kumaryashcse@gmail.com>
2024-02-13 22:49:46 +0530

    test startTransfer native token

--- test/ThirdwebPaymentsGateway.t.sol
+++ test/ThirdwebPaymentsGateway.t.sol
@@ -9,6 +9,33 @@ import { MockTarget } from "./utils/MockTarget.sol";
 import "lib/forge-std/src/console.sol";
 
 contract ThirdwebPaymentsGatewayTest is Test {
+    event TransferStart(
+        bytes32 indexed clientId,
+        address indexed sender,
+        bytes32 transactionId,
+        address tokenAddress,
+        uint256 tokenAmount
+    );
+
+    event TransferEnd(
+        bytes32 indexed clientId,
+        address indexed receiver,
+        bytes32 transactionId,
+        address tokenAddress,
+        uint256 tokenAmount
+    );
+
+    event FeePayout(
+        bytes32 indexed clientId,
+        address indexed sender,
+        address payoutAddress,
+        address tokenAddress,
+        uint256 feeAmount,
+        uint256 feeBPS
+    );
+
+    event OperatorChanged(address indexed previousOperator, address indexed newOperator);
+
     ThirdwebPaymentsGateway internal gateway;
     MockERC20 internal mockERC20;
     MockTarget internal mockTarget;
@@ -61,6 +88,10 @@ contract ThirdwebPaymentsGatewayTest is Test {
         }
     }
 
+    /*///////////////////////////////////////////////////////////////
+                        internal util functions
+    //////////////////////////////////////////////////////////////*/
+
     function _buildMockTargetCalldata(
         address _sender,
         address _receiver,
@@ -82,36 +113,42 @@ contract ThirdwebPaymentsGatewayTest is Test {
     }
 
     function _prepareAndSignData(
-        address _operator,
+        uint256 _operatorPrivateKey,
         bytes32 _clientId,
         bytes32 _transactionId,
         address _tokenAddress,
         uint256 _tokenAmount,
         address _forwardAddress,
-        bytes memory _targetCalldata,
-        ThirdwebPaymentsGateway.PayoutInfo[] memory _payouts
-    ) internal returns (bytes memory) {
-        bytes32 _payoutsHash = _hashPayoutInfo(_payouts);
-        bytes memory dataToHash = abi.encodePacked(
-            _clientId,
-            _transactionId,
-            _tokenAddress,
-            _tokenAmount,
-            _payoutsHash,
-            _forwardAddress,
-            _targetCalldata
-        );
+        bytes memory _targetCalldata
+    ) internal returns (bytes memory signature) {
+        bytes memory dataToHash;
+        {
+            bytes32 _payoutsHash = _hashPayoutInfo(payouts);
+            dataToHash = abi.encodePacked(
+                _clientId,
+                _transactionId,
+                _tokenAddress,
+                _tokenAmount,
+                _payoutsHash,
+                _forwardAddress,
+                _targetCalldata
+            );
+        }
 
-        bytes32 _hash = keccak256(dataToHash);
-        bytes32 ethSignedMsgHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", _hash));
+        {
+            bytes32 _hash = keccak256(dataToHash);
+            bytes32 ethSignedMsgHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", _hash));
+            (uint8 v, bytes32 r, bytes32 s) = vm.sign(_operatorPrivateKey, ethSignedMsgHash);
 
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(2, ethSignedMsgHash); // sign with operator private key, i.e. 2
-        bytes memory signature = abi.encodePacked(r, s, v);
-
-        return signature;
+            signature = abi.encodePacked(r, s, v);
+        }
     }
 
-    function test_transferStart() public {
+    /*///////////////////////////////////////////////////////////////
+                    Test `startTransfer` with ERC20
+    //////////////////////////////////////////////////////////////*/
+
+    function test_startTransfer_erc20() public {
         uint256 sendValue = 1 ether;
         uint256 sendValueWithFees = sendValue + (sendValue * totalFeeBps) / 10_000;
         bytes memory targetCalldata = _buildMockTargetCalldata(sender, receiver, address(mockERC20), sendValue, "");
@@ -123,14 +160,13 @@ contract ThirdwebPaymentsGatewayTest is Test {
         // generate signature
         bytes32 _transactionId = keccak256("transaction ID");
         bytes memory _signature = _prepareAndSignData(
-            operator,
+            2, // sign with operator private key, i.e. 2
             clientId,
             _transactionId,
             address(mockERC20),
             sendValue,
             address(mockTarget),
-            targetCalldata,
-            payouts
+            targetCalldata
         );
 
         // state/balances before sending transaction
@@ -157,5 +193,121 @@ contract ThirdwebPaymentsGatewayTest is Test {
         assertEq(mockERC20.balanceOf(client), clientBalanceBefore + (sendValue * clientFeeBps) / 10_000);
         assertEq(mockERC20.balanceOf(sender), senderBalanceBefore - sendValueWithFees);
         assertEq(mockERC20.balanceOf(receiver), receiverBalanceBefore + sendValue);
+    }
+
+    function test_startTransfer_nativeToken() public {
+        uint256 sendValue = 1 ether;
+        uint256 sendValueWithFees = sendValue + (sendValue * totalFeeBps) / 10_000;
+        bytes memory targetCalldata = _buildMockTargetCalldata(sender, receiver, address(0), sendValue, "");
+
+        // generate signature
+        bytes32 _transactionId = keccak256("transaction ID");
+        bytes memory _signature = _prepareAndSignData(
+            2, // sign with operator private key, i.e. 2
+            clientId,
+            _transactionId,
+            address(0),
+            sendValue,
+            address(mockTarget),
+            targetCalldata
+        );
+
+        // state/balances before sending transaction
+        uint256 ownerBalanceBefore = owner.balance;
+        uint256 clientBalanceBefore = client.balance;
+        uint256 senderBalanceBefore = sender.balance;
+        uint256 receiverBalanceBefore = receiver.balance;
+
+        // send transaction
+        vm.prank(sender);
+        gateway.startTransfer{ value: sendValueWithFees }(
+            clientId,
+            _transactionId,
+            address(0),
+            sendValue,
+            payouts,
+            payable(address(mockTarget)),
+            targetCalldata,
+            _signature
+        );
+
+        // check balances after transaction
+        assertEq(owner.balance, ownerBalanceBefore + (sendValue * ownerFeeBps) / 10_000);
+        assertEq(client.balance, clientBalanceBefore + (sendValue * clientFeeBps) / 10_000);
+        assertEq(sender.balance, senderBalanceBefore - sendValueWithFees);
+        assertEq(receiver.balance, receiverBalanceBefore + sendValue);
+    }
+
+    function test_startTransfer_events() public {
+        uint256 sendValue = 1 ether;
+        uint256 sendValueWithFees = sendValue + (sendValue * totalFeeBps) / 10_000;
+        bytes memory targetCalldata = _buildMockTargetCalldata(sender, receiver, address(mockERC20), sendValue, "");
+
+        // approve amount to gateway contract
+        vm.prank(sender);
+        mockERC20.approve(address(gateway), sendValueWithFees);
+
+        // generate signature
+        bytes32 _transactionId = keccak256("transaction ID");
+        bytes memory _signature = _prepareAndSignData(
+            2, // sign with operator private key, i.e. 2
+            clientId,
+            _transactionId,
+            address(mockERC20),
+            sendValue,
+            address(mockTarget),
+            targetCalldata
+        );
+
+        // send transaction
+        vm.prank(sender);
+        vm.expectEmit(true, true, false, true);
+        emit TransferStart(clientId, sender, _transactionId, address(mockERC20), sendValue);
+        gateway.startTransfer(
+            clientId,
+            _transactionId,
+            address(mockERC20),
+            sendValue,
+            payouts,
+            payable(address(mockTarget)),
+            targetCalldata,
+            _signature
+        );
+    }
+
+    function test_revert_startTransfer_invalidSignature() public {
+        uint256 sendValue = 1 ether;
+        uint256 sendValueWithFees = sendValue + (sendValue * totalFeeBps) / 10_000;
+        bytes memory targetCalldata = _buildMockTargetCalldata(sender, receiver, address(mockERC20), sendValue, "");
+
+        // approve amount to gateway contract
+        vm.prank(sender);
+        mockERC20.approve(address(gateway), sendValueWithFees);
+
+        // generate signature
+        bytes32 _transactionId = keccak256("transaction ID");
+        bytes memory _signature = _prepareAndSignData(
+            123, // sign with random private key
+            clientId,
+            _transactionId,
+            address(mockERC20),
+            sendValue,
+            address(mockTarget),
+            targetCalldata
+        );
+
+        // send transaction
+        vm.prank(sender);
+        vm.expectRevert("failed to verify transaction");
+        gateway.startTransfer(
+            clientId,
+            _transactionId,
+            address(mockERC20),
+            sendValue,
+            payouts,
+            payable(address(mockTarget)),
+            targetCalldata,
+            _signature
+        );
     }
 }
