98c5556f713d6c3921ae838e847813db9a75c0f8
Yash <kumaryashcse@gmail.com>
2024-02-13 18:44:39 +0530

    prettier

--- contracts/MockTarget.sol
+++ contracts/MockTarget.sol
@@ -5,67 +5,58 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "lib/forge-std/src/console.sol";
 
 contract MockTarget {
-  event TargetLog(
-    address sender,
-    address receiver,
-    address tokenAddress,
-    uint256 tokenAmount,
-    string message
-  );
+    event TargetLog(address sender, address receiver, address tokenAddress, uint256 tokenAmount, string message);
 
-  address constant private NATIVE_TOKEN_ADDRESS = 0x0000000000000000000000000000000000000000;
+    address private constant NATIVE_TOKEN_ADDRESS = 0x0000000000000000000000000000000000000000;
 
-  function decodeData(bytes memory data) private pure returns (address, address, address, uint256, string memory) {
-    return abi.decode(data, (address, address, address, uint256, string));
-  }
-
-  function performERC20Action(
-    address sender,
-    address payable receiver,
-    address tokenAddress,
-    uint256 tokenAmount,
-    string memory message
-  ) private {
-    emit TargetLog(sender, receiver, tokenAddress, tokenAmount, message);
-    console.log(
-      "Transferring %s erc20 tokens from %s to %s", tokenAmount, sender, receiver
-    );
-
-    require(IERC20(tokenAddress).transferFrom(msg.sender, receiver, tokenAmount), "Token transfer failed");
-  }
-
-  function performNativeTokenAction(
-    address sender,
-    address payable receiver,
-    address tokenAddress,
-    uint256 tokenAmount,
-    string memory message
-  ) private {
-    emit TargetLog(sender, receiver, tokenAddress, tokenAmount, message);
-    console.log(
-      "Transferring %s native tokens from %s to %s", tokenAmount, sender, receiver
-    );
-    (bool sent, ) = receiver.call{value: msg.value}("");
-    require(sent, "Failed to send Ether");
-  }
-
-  fallback() external payable {
-    require(msg.data.length > 0, "data required");
-    (address sender, address receiver, 
-      address tokenAddress, uint256 tokenAmount, 
-      string memory message
-    ) = decodeData(msg.data);
-
-    if(tokenAddress == NATIVE_TOKEN_ADDRESS)
-    {
-      console.log("Calling native token action!");
-      performNativeTokenAction(payable(sender), payable(receiver), tokenAddress, tokenAmount, message);
+    function decodeData(bytes memory data) private pure returns (address, address, address, uint256, string memory) {
+        return abi.decode(data, (address, address, address, uint256, string));
     }
-    else {
-      console.log("Calling erc20 token action!");
-      performERC20Action(payable(sender), payable(receiver), tokenAddress, tokenAmount, message);
-    }
-  }
 
-  receive() external payable {}
+    function performERC20Action(
+        address sender,
+        address payable receiver,
+        address tokenAddress,
+        uint256 tokenAmount,
+        string memory message
+    ) private {
+        emit TargetLog(sender, receiver, tokenAddress, tokenAmount, message);
+        console.log("Transferring %s erc20 tokens from %s to %s", tokenAmount, sender, receiver);
+
+        require(IERC20(tokenAddress).transferFrom(msg.sender, receiver, tokenAmount), "Token transfer failed");
+    }
+
+    function performNativeTokenAction(
+        address sender,
+        address payable receiver,
+        address tokenAddress,
+        uint256 tokenAmount,
+        string memory message
+    ) private {
+        emit TargetLog(sender, receiver, tokenAddress, tokenAmount, message);
+        console.log("Transferring %s native tokens from %s to %s", tokenAmount, sender, receiver);
+        (bool sent, ) = receiver.call{ value: msg.value }("");
+        require(sent, "Failed to send Ether");
+    }
+
+    fallback() external payable {
+        require(msg.data.length > 0, "data required");
+        (
+            address sender,
+            address receiver,
+            address tokenAddress,
+            uint256 tokenAmount,
+            string memory message
+        ) = decodeData(msg.data);
+
+        if (tokenAddress == NATIVE_TOKEN_ADDRESS) {
+            console.log("Calling native token action!");
+            performNativeTokenAction(payable(sender), payable(receiver), tokenAddress, tokenAmount, message);
+        } else {
+            console.log("Calling erc20 token action!");
+            performERC20Action(payable(sender), payable(receiver), tokenAddress, tokenAmount, message);
+        }
+    }
+
+    receive() external payable {}
 }
