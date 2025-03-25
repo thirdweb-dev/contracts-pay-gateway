da4903fd74a56b00e2500278a6a904da478cbf3f
Yash <kumaryashcse@gmail.com>
2025-03-25 20:38:57 +0530

    check impl code, other cleanup

--- src/UniversalGatewayProxy.sol
+++ src/UniversalGatewayProxy.sol
@@ -2,20 +2,34 @@
 pragma solidity ^0.8.22;
 
 contract UniversalGatewayProxy {
+    error ImplementationZeroAddress();
+    error ImplementationDoesNotExist();
     bytes32 private constant _ERC1967_IMPLEMENTATION_SLOT =
         0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
-    constructor(address _implementation, address _defaultAdmin, address payable _payoutAddress, uint256 _feeBps) {
-        require(_implementation != address(0), "Invalid implementation address");
+    constructor(
+        address _implementation,
+        address _owner,
+        address payable _protocolFeeRecipient,
+        uint256 _protocolFeeBps
+    ) {
+        if (_implementation == address(0)) {
+            revert ImplementationZeroAddress();
+        }
+
+        if (_implementation.code.length == 0) {
+            revert ImplementationDoesNotExist();
+        }
+
         assembly {
             sstore(_ERC1967_IMPLEMENTATION_SLOT, _implementation)
         }
 
         bytes memory data = abi.encodeWithSignature(
             "initialize(address,address,uint256)",
-            _defaultAdmin,
-            _payoutAddress,
-            _feeBps
+            _owner,
+            _protocolFeeRecipient,
+            _protocolFeeBps
         );
         (bool success, ) = _implementation.delegatecall(data);
         require(success, "Initialization failed");
